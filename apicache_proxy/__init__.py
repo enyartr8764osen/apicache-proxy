a499ed1ffca18ca42beb61253b1fa1481bd0c012
Enya Rosen <enyartr8764osen@users.noreply.github.com>
2026-05-14 22:02:03 +0800

    Add get_version() helper function to public API

--- apicache_proxy/__init__.py
+++ apicache_proxy/__init__.py
@@ -4,3 +4,17 @@ from apicache_proxy.cache import FileCache, CacheEntry, DEFAULT_CACHE_DIR, DEFAU
 
 __all__ = ["FileCache", "CacheEntry", "DEFAULT_CACHE_DIR", "DEFAULT_TTL"]
 __version__ = "0.1.0"
+
+
+def get_version() -> str:
+    """Return the current version of apicache-proxy.
+
+    Returns:
+        str: The version string in semver format.
+
+    Example:
+        >>> import apicache_proxy
+        >>> apicache_proxy.get_version()
+        '0.1.0'
+    """
+    return __version__
